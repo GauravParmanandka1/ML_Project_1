3eac1eeaa0d5c3219c4ff561cf15dff6ab034216
Gaurav Parmanandka <gaurav.parmanandka@gmail.com>
2025-02-23 00:17:02 +0530

    Fixed the Custom Exception file and Logger file for minor issues

--- src/logger.py
+++ src/logger.py
@@ -3,7 +3,7 @@ import os
 from datetime import datetime
 
 LOG_File=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path=os.path.join(os.getcwd(),'logs',LOG_File)
+logs_path=os.path.join(os.getcwd(),'logs')
 os.makedirs(logs_path,exist_ok=True)
 
 LOG_FILE_PATH=os.path.join(logs_path,LOG_File)
